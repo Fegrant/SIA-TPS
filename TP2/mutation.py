d42f7cfe0f5a3ec644a33508c8755cc81ffb4527
VALENTIN YE LI <yeliv9527@gmail.com>
2023-04-13 12:02:18 -0300

    mutation complete

--- TP2/mutation.py
+++ TP2/mutation.py
@@ -1,6 +1,10 @@
 import random
 from color import Color
 
+# La funcion de mutacion en este caso es aleatoria, 
+# se se elige un valor aleatorio para una posición aleatoria del cromosoma. 
+# Por ejemplo, podrías cambiar el valor de un componente RGB por un valor aleatorio entre 0 y 255.
+
 def gene_mutation(individual: Color, mutation_probability: float, gene: int):
     if random.uniform(0, 1) <= mutation_probability:
         new_locus = random.randint(0, 255)
@@ -17,11 +21,15 @@ def limited_multiple_gene_mutation(individual: Color, gens_to_mutate: int, mutat
     return
 
 
-def uniform_multiple_gene_mutation():
+def uniform_multiple_gene_mutation(individual: Color, mutation_probability: float):
+    for pos in range(3):
+        if random.uniform(0, 1) <= mutation_probability:
+            new_locus = random.randint(0, 255)
+            individual.rgb[pos] = new_locus
+    return
 
-    return 
 
 
-def complete_mutation():
-
-    return 
+def complete_mutation(individual: Color, mutation_probability: float):
+    if random.uniform(0, 1) <= mutation_probability:
+        individual.rgb = [random.randint(0, 255) for _ in range(3)]
