610a31f634c3e7543b7ddaefc5cc50c5c7c86116
Hugo LICHTENBERGER <76788589+LICHTENBERGERHugo@users.noreply.github.com>
2023-04-30 15:59:58 -0300

    Cleaning git tree

--- TP3/perceptron.py
+++ TP3/perceptron.py
@@ -1,36 +1,41 @@
 import numpy as np
 
+
 class Perceptron:
     def __init__(self, num_inputs, learning_rate=0.01, max_epochs=1000):
         self.weights = np.zeros(num_inputs + 1)     # Extra for w0
         self.learning_rate = learning_rate
         self.max_epochs = max_epochs
-    
-    def heaviside_predict(self, input):                 # input might be a vector or scalar
+
+    # input might be a vector or scalar
+    def heaviside_predict(self, input):
         value = np.dot(input, self.weights[1:]) + self.weights[0]
         return np.where(value >= 0, 1, 0)
-    
+
     def train(self, X, y):
-        for epoch in np.arange(self.max_epochs):
-            for input, solution in zip(X, y):           # zip(X, y) makes tuples of (input, solution) to iterate them
+        for _ in np.arange(self.max_epochs):
+            # zip(X, y) makes tuples of (input, solution) to iterate them
+            for input, solution in zip(X, y):
                 prediction = self.heaviside_predict(input)
-                
+
                 error = solution - prediction
                 input_approximation = self.learning_rate * error
 
                 self.weights[0] += input_approximation
                 self.weights[1:] += input_approximation * input
-            
+
             prediction = self.heaviside_predict(X)
             if np.array_equal(y, prediction):
                 break               # Finish by convergence
-            
+
             if self.perceptron_accuracy(X, y) == 100:
                 break               # Finish by 100% accuracy on predicts
-    
+
     def perceptron_accuracy(self, X, y):
         prediction = self.heaviside_predict(X)
-        return 100 * np.mean(prediction == y)       # Checks if all values of prediction and y are equal
+        # Checks if all values of prediction and y are equal
+        return 100 * np.mean(prediction == y)
+
 
 # Creation, training and values of and perceptron
 and_perceptron = Perceptron(2)
