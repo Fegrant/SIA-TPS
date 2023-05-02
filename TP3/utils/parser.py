5204d015053d6fa783ec2acbfd5de22574cff442
Fegrant <nicolasbirsa333@gmail.com>
2023-05-02 14:28:45 -0300

    fix(ex3): arrays now parsed with solutions

--- TP3/utils/parser.py
+++ TP3/utils/parser.py
@@ -7,5 +7,10 @@ def parse_csv_file(file: str):
 
 def parse_txt_file(file: str):
     df = pd.read_csv(file, header=None, sep=' ', usecols=[0, 1, 2, 3, 4])
-    arr = df.to_numpy()
-    return np.split(arr, 10)
+    read_matrix = df.to_numpy()
+    read_matrix = np.split(read_matrix, 10)
+    parsed_matrix = np.array([])
+    for i in np.arange(10):
+        parsed_matrix.append((read_matrix[0], i))
+    print(parsed_matrix)
+    return parsed_matrix
