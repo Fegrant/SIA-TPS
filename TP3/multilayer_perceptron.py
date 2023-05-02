ca37a1bde5ff3748631cce3c02b322461bb6d339
VALENTIN YE LI <yeliv9527@gmail.com>
2023-05-02 18:58:12 -0300

    wip mlp

--- TP3/multilayer_perceptron.py
+++ TP3/multilayer_perceptron.py
@@ -1,35 +1,42 @@
 import numpy as np
 
 class MultilayerPerceptron:
-    def __init__(self, n_inputs, n_hidden, n_outputs):
+    def __init__(self, n_inputs, hidden_layers, n_outputs):
         self.n_inputs = n_inputs
-        self.n_hidden = n_hidden
+        self.hidden_layers = hidden_layers
         self.n_outputs = n_outputs
         self.init_weights()
-        self.init_biases()
 
+    # initialize weights with random values
     def init_weights(self):
-        self.weights = np.zeros(self.n_inputs) 
-    
-    def init_biases(self):
-        self.biases = np.ones(self.n_inputs, 1)
-    
-    def train(self):
+        self.weights = np.random.randn(self.n_inputs, self.hidden_layers[0])
+        for i in range(1, len(self.hidden_layers)):
+            self.weights = np.concatenate((self.weights, np.random.randn(self.hidden_layers[i-1], self.hidden_layers[i])), axis=1)
+        self.weights = np.concatenate((self.weights, np.random.randn(self.hidden_layers[-1], self.n_outputs)), axis=1)
+        
+    def fit(self, X, y, epochs=1000, learning_rate=0.1, beta=0.1):
+        for epoch in range(epochs):
+            for input, solution in zip(X, y):
+                prediction = self.feedforward(input)
+                self.backpropagation(input, solution, prediction, learning_rate, beta)
+                
+
+    def feedforward(self, X):
         pass
+
+    def backpropagation(self, X, y):
+        pass
+
 
     def predict(self, X):
-        X = np.array(X).reshape(self.n_inputs, 1)
-        return np.round(self.feedforward(X))
+        pass
 
-    def activation(self, x):
+    def sigmoid_activation(self, x):
         return 1 / (1 + np.exp(-x))
 
-    def activation_derivative(self, x):
-        return self.activation(x) * (1 - self.activation(x))
+    def sigmoid_activation_derivative(self, x):
+        return self.sigmoid_activation(x) * (1 - self.sigmoid_activation(x))
         
-    def feedforward(self, X):
-        hidden_output = self.activation(np.dot(self.weights, X) + self.biases)
-        return hidden_output
 
-    def backpropagation(self):
-        pass
+    def mse(self, target, prediction):
+        return np.square(np.subtract(target, prediction)).mean()
