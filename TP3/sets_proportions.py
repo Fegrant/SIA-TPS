03ef3e4c22e1eedff8564cee9b25eba95a6d6bce
apodgornyitba <apodgorny@itba.edu.ar>
2023-05-03 17:58:08 -0300

    Add: Errorbars ploting

--- TP3/sets_proportions.py
+++ TP3/sets_proportions.py
@@ -1,5 +1,5 @@
 import copy
-from statistics import mean
+from statistics import mean, stdev
 from utils.parser import parse_csv_file
 from perceptron import SimpleLinealPerceptron, SimpleNonLinealPerceptron, ActivationFunc
 from config import load_config, ex2_test_size
@@ -13,11 +13,17 @@ from sklearn.model_selection import train_test_split
 testSetProportions = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
 resultsMap = {}
 averageListTanhTest = []
+semAverageListTanhTest = []
 averageListTanhTraining = []
+semAverageListTanhTraining = []
 averageListLogisticTest = []
+semAverageListLogisticTest = []
 averageListLogisticTraining = []
+semAverageListLogisticTraining = []
 averageListLinealTest = []
+semAverageListLinealTest = []
 averageListLinealTraining = []
+semAverageListLinealTraining = []
 
 input = parse_csv_file('input_files/TP3-ej2-conjunto.csv')
 perceptron_config = load_config()
@@ -43,7 +49,7 @@ for proportion in testSetProportions:
                                   'lineal': {'train': [], 'test':[]}
                                 }
 
-for i in range(5):
+for i in range(2):
         for testPercentage in testSetProportions:
                 input_train, input_test = train_test_split(input, test_size=testPercentage)
                 
@@ -56,20 +62,20 @@ for i in range(5):
                 inputCopy = copy.deepcopy(input)
 
                 lineal_perceptron = SimpleLinealPerceptron(lineal_num_inputs, lineal_learning_rate, lineal_epochs, lineal_accepted_error)
-                train_lineal_mse = lineal_perceptron.train(X_train, y_train)
-                test_lineal_mse = lineal_perceptron.train(X_test, y_test)
+                train_lineal_mse, errors = lineal_perceptron.train(X_train, y_train)
+                test_lineal_mse, errors = lineal_perceptron.train(X_test, y_test)
         
                 non_lineal_logistic_perceptron = SimpleNonLinealPerceptron(no_lineal_num_inputs, no_lineal_learning_rate, no_lineal_epochs, no_lineal_accepted_error, beta, ActivationFunc.LOGISTIC)
                 ynorm_logistic = feature_scaling(y_train, 0, 1)
                 ynorm_test_logistic = feature_scaling(y_test, 0, 1)
-                train_logistic_mse = non_lineal_logistic_perceptron.train(X_train, ynorm_logistic)
-                test_logistic_mse = non_lineal_logistic_perceptron.train(X_test, ynorm_test_logistic)
+                train_logistic_mse, errors= non_lineal_logistic_perceptron.train(X_train, ynorm_logistic)
+                test_logistic_mse, errors = non_lineal_logistic_perceptron.train(X_test, ynorm_test_logistic)
                 
                 non_lineal_tanh_perceptron = SimpleNonLinealPerceptron(no_lineal_num_inputs, no_lineal_learning_rate, no_lineal_epochs, no_lineal_accepted_error, beta, ActivationFunc.TANH)
                 ynorm_tanh = feature_scaling(y_train, -1, 1)
                 ynorm_test_tanh = feature_scaling(y_test, -1, 1)
-                train_tanh_mse = non_lineal_tanh_perceptron.train(X_train, ynorm_tanh)
-                test_tanh_mse = non_lineal_tanh_perceptron.train(X_test, ynorm_test_tanh)
+                train_tanh_mse, errors = non_lineal_tanh_perceptron.train(X_train, ynorm_tanh)
+                test_tanh_mse, errors = non_lineal_tanh_perceptron.train(X_test, ynorm_test_tanh)
 
                 resultsMap[testPercentage][ActivationFunc.LOGISTIC]['train'].append(train_logistic_mse)
                 resultsMap[testPercentage][ActivationFunc.TANH]['train'].append(train_tanh_mse)
@@ -80,39 +86,45 @@ for i in range(5):
         
 for proportion in testSetProportions:
         averageListLogisticTest.append(mean(resultsMap[proportion][ActivationFunc.LOGISTIC]['test']))
+        semAverageListLogisticTest.append(stdev(resultsMap[proportion][ActivationFunc.LOGISTIC]['test']))
         averageListLogisticTraining.append(mean(resultsMap[proportion][ActivationFunc.LOGISTIC]['train']))
+        semAverageListLogisticTraining.append(stdev(resultsMap[proportion][ActivationFunc.LOGISTIC]['train']))
         averageListTanhTest.append(mean(resultsMap[proportion][ActivationFunc.TANH]['test']))
+        semAverageListTanhTest.append(stdev(resultsMap[proportion][ActivationFunc.TANH]['test']))
         averageListTanhTraining.append(mean(resultsMap[proportion][ActivationFunc.TANH]['train']))
+        semAverageListTanhTraining.append(stdev(resultsMap[proportion][ActivationFunc.TANH]['train']))
         averageListLinealTest.append(mean(resultsMap[proportion]['lineal']['test']))
+        semAverageListLinealTest.append(stdev(resultsMap[proportion]['lineal']['test']))
         averageListLinealTraining.append(mean(resultsMap[proportion]['lineal']['train']))
+        semAverageListLinealTraining.append(stdev(resultsMap[proportion]['lineal']['train']))
 
 width = 0.3
 x = np.arange(len(testSetProportions))
 plt.xlabel("Test set %", fontsize=12)
 plt.ylabel("MSE", fontsize=12)
 plt.title("Error on both Sets (Tanh)")
-plt.bar(x, averageListTanhTest,width, label='Test')
-plt.bar(x+width, averageListTanhTraining, width,label ='Training')
-plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
+plt.errorbar(x, averageListTanhTest, yerr=semAverageListTanhTest, marker='o', label='Test')
+plt.errorbar(x+width, averageListTanhTraining, yerr=semAverageListTanhTest, marker='o', label ='Training')
 plt.legend()
+plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
 plt.show()
 
 plt.xlabel("Test set %", fontsize=12)
 plt.ylabel("MSE", fontsize=12)
 plt.title("Error on both Sets (Logistica)")
-plt.bar(x, averageListLogisticTest, width, label='Test')
-plt.bar(x+width, averageListLogisticTraining, width, label='Training')
-plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
+plt.errorbar(x, averageListLogisticTest, yerr=semAverageListLogisticTest, marker='o', label='Test')
+plt.errorbar(x+width, averageListLogisticTraining, yerr=semAverageListLogisticTest, marker='o', label ='Training')
 plt.legend()
+plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
 plt.show()
 
 plt.xlabel("Test set %", fontsize=12)
 plt.ylabel("MSE", fontsize=12)
 plt.title("Error on both Sets (Lineal)")
-plt.bar(x, averageListLinealTest, width, label='Test')
-plt.bar(x+width, averageListLinealTraining,width, label='Training')
-plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
+plt.errorbar(x, averageListLinealTest, yerr=semAverageListLinealTest, marker='o', label='Test')
+plt.errorbar(x+width, averageListLinealTraining, yerr=semAverageListLinealTest, marker='o', label ='Training')
 plt.legend()
+plt.xticks(x + width / 2, ('10', '20', '30','40','50','60','70','80'))
 plt.show()
 
 
