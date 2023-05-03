8c494d1b73bd2b652fbff4317ca9e675d800546e
Fegrant <nicolasbirsa333@gmail.com>
2023-05-03 16:20:49 -0300

    wip: multilayer perceptron files

--- TP3/multilayer_perceptron.py
+++ TP3/multilayer_perceptron.py
@@ -2,23 +2,33 @@ import numpy as np
 import copy
 
 class MultilayerPerceptron:
-    def __init__(self, n_inputs, hidden_layers, n_outputs, momentum=None):
+    def __init__(self, n_inputs, hidden_layer_amount, n_outputs, momentum=None):
         self.n_inputs = n_inputs
-        self.hidden_layers = hidden_layers
+        self.hidden_layer_amount = hidden_layer_amount
         self.n_outputs = n_outputs
         self.momentum = momentum
         self.init_weights()
 
     # initialize weights with random values
     def init_weights(self):
-        self.weights = np.random.randn(self.n_inputs, self.hidden_layers[0])
-        print(self.weights)
-        for i in range(1, len(self.hidden_layers)):
-            self.weights = np.concatenate((self.weights, np.random.randn(self.hidden_layers[i], self.hidden_layers[i-1])), axis=0)
-        self.weights = np.concatenate((self.weights, np.random.randn(self.n_outputs, self.hidden_layers[-1])), axis=0)
-        print(self.weights)
+        self.weights_per_layer = []
+
+        # Initialize the weights of the first hidden layer
+        weights = np.random.randn(self.hidden_layer_amount[0], self.n_inputs + 1)
+        self.weights_per_layer.append(weights)
+
+        # Initialize the weights of the next hidden layers
+        for i in range(1, len(self.hidden_layer_amount)):
+            weights = np.random.randn(self.hidden_layer_amount[i], self.hidden_layer_amount[i-1] + 1)
+            self.weights_per_layer.append(weights)
+        
+        # Initialize the weights of the output layer
+        weights = np.random.randn(self.n_outputs, self.hidden_layer_amount[-1] + 1)
+        self.weights_per_layer.append(weights)
+
         if self.momentum is not None:
-            self.prev_weights = copy.deepcopy(self.weights)
+            self.prev_weights_per_layer = copy.deepcopy(self.weights_per_layer)
+
 
     def fit(self, X, y, epochs=1000, learning_rate=0.1, beta=0.1, momentum=None):
         self.beta = beta
@@ -30,26 +40,32 @@ class MultilayerPerceptron:
         sum_errors = np.array([], dtype=np.float64)
 
         # for each epoch train the network on each sample and update weights
-        for epoch in range(epochs):
+        for epoch in np.arange(epochs):
             # Iterate over the samples
-            for i in range(X.shape[0]):
+            for i in np.arange(X.shape[0]):
                 # Feedforward the input and compute the activations
                 self.feedforward(X[i])
                 # Backpropagate the error and update the weights
                 self.backpropagation(X[i], y[i], learning_rate)
             
             # Compute the MSE after each epoch and append it to the list (for plotting purposes, each epoch is a point in the plot)
-            sum_errors = np.append(sum_errors, np.sum((y - self.feedforward(X))**2)/X.shape[0])
-        
+            sum_errors = np.append(sum_errors, self.mse(y, self.predict(X)))
 
 
     # feedforward the input through the network and return the output layer
     def feedforward(self, X):
-        self.activations = [X]
-        for i in range(len(self.hidden_layers) + 1):
-            print(self.activations[i])
-            print(self.weights[i])
-            self.activations.append(self.sigmoid_activation(np.dot(self.activations[i], self.weights[i])))
+        # Add the bias to the input layer
+        self.activations = [np.insert(X, 0, 1)]
+
+        # Compute the activations of the hidden layers
+        for i in np.arange(len(self.hidden_layer_amount)):
+            activation = self.sigmoid_activation(np.dot(self.activations[i], self.weights_per_layer[i].T))
+            self.activations.append(np.insert(activation, 0, 1))
+
+        # Compute the activations of the output layer
+        activation = self.sigmoid_activation(np.dot(self.activations[-1], self.weights_per_layer[-1].T))
+        self.activations.append(activation)
+
         return self.activations[-1]
         
 
@@ -57,50 +73,26 @@ class MultilayerPerceptron:
     def backpropagation(self, X, Y, learning_rate=0.1):
         # Compute the error at the output layer
         output_error = Y - self.activations[-1]
+        layer_deltas = []
 
         # Compute the gradient at the output layer
-        output_layer_gradient = output_error * self.sigmoid_activation_derivative(self.activations[-1])
+        last_layer_delta = output_error * self.sigmoid_activation_derivative(self.activations[-1])
+        layer_deltas.append(last_layer_delta)
 
         # Compute the gradient at the hidden layers
-        hidden_layer_gradients = []
-        for i in range(len(self.hidden_layers), 0, -1):
-            print("hola xd")
-            print(hidden_layer_gradients)
-            if i == len(self.hidden_layers):
-                gradient = np.dot(output_layer_gradient, self.weights[:, sum(self.hidden_layers[:-1]):].T) * self.sigmoid_activation_derivative(self.activations[i])
-            else:
-                gradient = np.dot(hidden_layer_gradients[-1], self.weights[:, sum(self.hidden_layers[i:]):sum(self.hidden_layers[i+1:])].T) * self.sigmoid_activation_derivative(self.activations[i])
-            hidden_layer_gradients.append(gradient)
+        
+        for i in np.arange(len(self.hidden_layer_amount), 0, -1):
+            output_error = np.dot(layer_deltas, self.weights_per_layer[i+1])
+            layer_delta = output_error * self.sigmoid_activation_derivative(self.activations[i]) 
+            layer_deltas.append(layer_delta)
+            # last_layer_delta = layer_deltas
 
-        # Reverse the order of the gradients for convenience
-        hidden_layer_gradients.reverse()
+        # Update the weights of the output layer
+        self.weights_per_layer[-1] += learning_rate * np.outer(last_layer_delta, self.activations[-2])
 
-        print("Hola")
-        print(hidden_layer_gradients)
+            
+        
 
-        # Update the weights using the computed gradients
-        for i in range(len(self.hidden_layers)+1):
-            if i == 0:
-                print(X.T)
-                print(hidden_layer_gradients[i])
-                weight_update = np.dot(X.T, hidden_layer_gradients[i])
-            elif i == len(self.hidden_layers):
-                weight_update = np.dot(self.activations[-2].T, output_layer_gradient)
-            else:
-                weight_update = np.dot(self.activations[i-1].T, hidden_layer_gradients[i])
-            if self.momentum is not None:
-                weight_update = self.momentum * self.prev_weights[:, sum(self.hidden_layers[:i]):sum(self.hidden_layers[:i+1])] + (1 - self.momentum) * weight_update
-            self.weights[:, sum(self.hidden_layers[:i]):sum(self.hidden_layers[:i+1])] += weight_update * learning_rate
-        if self.momentum is not None:
-            self.prev_weights = copy.deepcopy(self.weights)
-
-    
-    # update the weights
-    def update_weights(self, weight_updates, learning_rate, momentum=None):
-        if momentum is not None:
-            weight_updates = momentum * self.prev_weights + (1 - momentum) * weight_updates
-            self.prev_weights = weight_updates
-        self.weights += learning_rate * weight_updates
 
     def predict(self, X):
         return self.feedforward(X)
