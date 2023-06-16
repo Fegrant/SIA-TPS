f331ea5116f4035718f5eda1ebd1773acc243fa0
Fegrant <nicolasbirsa333@gmail.com>
2023-06-16 14:03:49 -0300

    feat: add autoencoder code

--- TP5/autoencoder.py
+++ TP5/autoencoder.py
@@ -5,27 +5,58 @@ from config import load_config_multilayer
 import numpy as np
 import matplotlib.pyplot as plt
 
-letters, labels = load_data('inputs/font.json')
-monocromatic_cmap = plt.get_cmap('binary')
-plt.imshow(to_bin_array(letters[0]), cmap=monocromatic_cmap)
-plt.show()
+def is_same_letter(originals: list[float], predictions: list[float], max_errors=1):
+    wrong_letters = []
+    wrong_predictions = []
+    for i in range(len(originals)):
+        errors = 0
+        letter = originals[i]
+        letter_pred = predictions[i]
+        for j in range(len(letter)):
+            if letter[j] != int(letter_pred[j]):
+                errors += 1
+                if errors > max_errors:
+                    wrong_letters.append(i)
+                    wrong_predictions.append(letter_pred)
+                    break
+    return wrong_letters, wrong_predictions
 
-# input = letters
-# output = letters
+# letters, labels = load_data('inputs/font.json')
+# monocromatic_cmap = plt.get_cmap('binary')
+# plt.imshow(to_bin_array(letters[2]), cmap=monocromatic_cmap)
+# plt.show()
 
-# multi_layer_perceptron_config = load_config_multilayer()
-# num_inputs = int(multi_layer_perceptron_config["number_of_inputs"])
-# num_outputs = int(multi_layer_perceptron_config["number_of_outputs"])
-# epochs = int(multi_layer_perceptron_config["epochs"])
-# learning_rate = float(multi_layer_perceptron_config["learning_rate"])
-# beta = float(multi_layer_perceptron_config["beta"])
-# hidden_layers = multi_layer_perceptron_config["hidden_layers"]
-# momentum = float(multi_layer_perceptron_config["momentum"])
+autoencoder = MultilayerPerceptron([35, 22, 10, 2, 10, 22, 35], 0.9)
+letters, labels = load_data_as_bin_array('inputs/font.json')
+# print(letters)
+autoencoder.train(letters, letters, 20000, 0.005)
 
-# mlp = MultilayerPerceptron([num_inputs] + hidden_layers + [num_outputs], momentum)
+# print("a: ", letters[1].reshape(7, 5))
+# print("a_pred: ", np.around(autoencoder.predict(letters[1]).reshape(7, 5), 3))
+# print('MSE: {}'.format(autoencoder.mse(autoencoder.predict(letters[1]), letters[1])))
+# print(autoencoder.predict(letters[1]))
 
-# mlp.train(input, output, epochs, learning_rate)
+wrong_letters, predictions = is_same_letter(letters, np.around(autoencoder.predict(letters), 0))
 
-# print("Errors: ", mlp.errors)
+print(wrong_letters)
 
+# if len(wrong_letters) >= 0:
+#     for i in range(len(wrong_letters)):
+#         print('Wrong letter: {}'.format(labels[wrong_letters[i]]))
+#         print('Original:\n{}'.format(letters[wrong_letters[i]].reshape(7, 5)))
+#         print('Predicted:\n{}'.format(predictions[i].reshape(7, 5)))
+#         print()
+# else:
+#     print('All letters were guessed correctly')
 
+# print("b prediction")
+# print("b: ", autoencoder.predict(letters[2]))
+# print('MSE: {}'.format(autoencoder.mse(autoencoder.predict(letters[2]), letters[2])))
+
+# print("c prediction")
+# print("c: ", autoencoder.predict(letters[3]))
+# print('MSE: {}'.format(autoencoder.mse(autoencoder.predict(letters[3]), letters[3])))
+
+# print("d prediction")
+# print("d: ", autoencoder.predict(letters[4]))
+# print('MSE: {}'.format(autoencoder.mse(autoencoder.predict(letters[4]), letters[4])))
--- TP5/utils/parser.py
+++ TP5/utils/parser.py
@@ -5,7 +5,7 @@ def load_data(path):
     with open(path, 'r') as f:
         data = json.load(f)
     labels=['`', 'a', 'b', 'c' ,'d', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n' ,'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' ,'z', '{', '}', '|', '~']
+            'n' ,'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y' ,'z', '{',  '|', '}', '~', 'DEL']
     return data['font'], labels
 
 def to_bin_array(encoded_caracter):
@@ -15,12 +15,12 @@ def to_bin_array(encoded_caracter):
         for col in range(0, 5):
             bin_array[row][4-col] = current_row & 1
             current_row >>= 1
-    return bin_array
+    return bin_array.flatten()
 
 def load_data_as_bin_array(path):
     letters, labels = load_data(path)
     bin_letters = []
     for letter in letters:
         bin_letters.append(to_bin_array(letter))
-    return bin_letters, labels
+    return np.array(bin_letters), labels
 
